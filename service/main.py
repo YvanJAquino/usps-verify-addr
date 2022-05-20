8353f2c735216c6665f8059a9916217355af8ec3
Yvan J. Aquino <yvan.aquino@cloud-colosseum.net>
2022-05-20 15:59:54 +0000

    LICENSE

--- service/main.py
+++ service/main.py
@@ -1,11 +1,22 @@
+# Copyright 2022 Google LLC
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import json
 import googlemaps
 from fastapi import FastAPI
 from cxwebhooks import WebhookRequest, WebhookResponse
-
-# Webhook.site [TESTING]: https://webhook.site/#!/de2a6e79-94bc-43bf-b571-5affeaf4f2ca
-# Webhook.site [TESTING] - Endpoint: https://webhook.site/de2a6e79-94bc-43bf-b571-5affeaf4f2ca
 
 # https://googlemaps.github.io/google-maps-services-python/docs/index.html
 
